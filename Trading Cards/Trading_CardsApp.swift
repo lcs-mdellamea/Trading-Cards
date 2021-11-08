3083da4b96635421fb5dea8e08ec62eb1c706ad1
Madison Dellamea <mdellamea@lcs.on.ca>
2021-11-08 11:41:46 -0500

    Initial Commit

---
+++ Trading Cards/Trading_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_CardsApp.swift
+//  Trading Cards
+//
+//  Created by Madison Dellamea on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Trading_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
