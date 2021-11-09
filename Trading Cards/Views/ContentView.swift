15cab5c1440306e44edd13c48de64913b16d33fe
Madison Dellamea <mdellamea@lcs.on.ca>
2021-11-08 20:58:31 -0500

    Changed layout colors, and added HStack w/ nested VStack.

--- Trading Cards/Views/ContentView.swift
+++ Trading Cards/Views/ContentView.swift
@@ -10,37 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-    
-                ZStack {
-                    VStack{
-                        Color.blue
-                            .padding(.horizontal,16)
-                            .frame(height: 100)
-                            .position(x:207, y: 65)
-                        Color.red
-                            .padding(.horizontal,16)
-                            .frame(height: 100)
-                            .position(x:207, y: 65)
-                        Color.white
-                            .padding(.horizontal, 16)
-                            .frame(height:50)
-                        Color.blue
-                            .padding(.horizontal,16)
-                            .frame(height: 85)
-                            .position(x: 207, y: 55)
-                    }
-                    Image("Undy")
-                        .resizable()
-                        .border(Color.black, width: 5)
-                        .padding()
-                        .scaledToFit()
+            Color.brown
+            ZStack {
+                Color.white
+                VStack {
+                    Color.indigo
+                        .padding(.horizontal,16)
+                        .frame(height: 100)
+                        .position(x:207, y: 65)
+                    Color.red
+                        .padding(.horizontal,16)
+                        .frame(height: 100)
+                        .position(x:207, y: 65)
+                    Color.white
+                        .padding(.horizontal, 16)
+                        .frame(height:50)
+                    Color.indigo
+                        .padding(.horizontal,16)
+                        .frame(height: 85)
+                        .position(x: 207, y: 55)
+}
+                Image("Undy")
+                    .resizable()
+                    .border(Color.black, width: 5)
+                    .padding()
+                    .scaledToFit()
+}
+            Color.brown
+            
+            HStack {
+               
+                VStack {
+                    Text("cdfvsd")
+                        .position(x: 40, y:20)
             }
-                
-              //  Text("cdfvsd")
         }
-        .navigationTitle("Undy")
+
     }
+        .navigationTitle("Undy")
 }
 
 struct ContentView_Previews: PreviewProvider {
